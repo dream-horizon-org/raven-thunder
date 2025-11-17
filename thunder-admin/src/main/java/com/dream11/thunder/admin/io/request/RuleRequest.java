2cfc2d2c0953f6ec45d1249295e9090c8b322415
ANIRAJ168 <aniket.raj@dream11.com>
2025-11-17 12:51:27 +0530

    thunde-admin unit tests done

--- thunder-admin/src/main/java/com/dream11/thunder/admin/io/request/RuleRequest.java
+++ thunder-admin/src/main/java/com/dream11/thunder/admin/io/request/RuleRequest.java
@@ -9,9 +9,14 @@ import java.util.Map;
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class RuleRequest {
   @NotNull @Valid private CohortEligibility cohortEligibility;
   @NotEmpty Map<String, String> stateToAction;
