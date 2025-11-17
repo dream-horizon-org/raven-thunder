8a11d818ba8f29fa4f20e84e08fb236529d81c9f
ANIRAJ168 <aniket.raj@dream11.com>
2025-11-17 13:19:56 +0530

    added tests for thunder-api and thunde-core

---
+++ thunder-api/src/test/java/com/dream11/thunder/api/service/cache/StaticDataCacheImplTest.java
@@ -0,0 +1,59 @@
+package com.dream11.thunder.api.service.cache;
+
+import com.dream11.thunder.core.dao.BehaviourTagsRepository;
+import com.dream11.thunder.core.dao.CTARepository;
+import com.dream11.thunder.core.model.BehaviourTag;
+import com.dream11.thunder.core.model.CTA;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Single;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class StaticDataCacheImplTest {
+
+  @Mock private CTARepository ctaRepository;
+  @Mock private BehaviourTagsRepository behaviourTagsRepository;
+
+  @InjectMocks private StaticDataCacheImpl cache;
+
+  @Test
+  void initiateCache_loadsAllDatasets_andFindReturnsCopies() {
+    // active
+    CTA active = new CTA();
+    active.setId(1L);
+    // paused
+    CTA paused = new CTA();
+    paused.setId(2L);
+    // tags
+    BehaviourTag tag = new BehaviourTag();
+    tag.setName("bt1");
+
+    when(ctaRepository.findAllWithStatusActive()).thenReturn(Single.just(Map.of(1L, active)));
+    when(ctaRepository.findAllWithStatusPaused()).thenReturn(Single.just(Map.of(2L, paused)));
+    when(behaviourTagsRepository.findAll()).thenReturn(Single.just(Map.of("bt1", tag)));
+
+    cache.initiateCache().join();
+
+    Map<Long, CTA> activeOut = cache.findAllActiveCTA();
+    Map<Long, CTA> pausedOut = cache.findAllPausedCTA();
+    Map<String, BehaviourTag> tagsOut = cache.findAllBehaviourTags();
+
+    assertThat(activeOut).containsOnlyKeys(1L);
+    assertThat(pausedOut).containsOnlyKeys(2L);
+    assertThat(tagsOut).containsOnlyKeys("bt1");
+
+    // Ensure copies returned (mutation does not affect cache)
+    activeOut.clear();
+    assertThat(cache.findAllActiveCTA()).containsOnlyKeys(1L);
+  }
+}
+
+
