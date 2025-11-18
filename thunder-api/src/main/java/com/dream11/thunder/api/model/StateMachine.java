18daa263f291b0a88ff4f45ae8cff0ce264caa05
ANIRAJ168 <aniket.raj@dream11.com>
2025-11-18 15:13:35 +0530

    added constructors

--- thunder-api/src/main/java/com/dream11/thunder/api/model/StateMachine.java
+++ thunder-api/src/main/java/com/dream11/thunder/api/model/StateMachine.java
@@ -3,10 +3,15 @@ package com.dream11.thunder.api.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import java.util.Map;
 import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
+@AllArgsConstructor
 public class StateMachine {
 
   @NotNull private String currentState;
