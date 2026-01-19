eb0288f9e1726e2c2937de9c43a0aa3f47837874
ANIRAJ168 <aniket.raj@dream11.com>
2026-01-19 17:43:53 +0530

    added ignore unknown same as internal repo

--- thunder-core/src/main/java/com/raven/thunder/core/model/CTA.java
+++ thunder-core/src/main/java/com/raven/thunder/core/model/CTA.java
@@ -6,12 +6,15 @@ import java.util.List;
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class CTA {
   @NotNull private Long id;
   private Rule rule;
@@ -20,7 +23,7 @@ public class CTA {
   private String description;
   private List<String> tags;
   private String team;
-  private List<String> behaviourTags = Collections.emptyList();
+  @Default private List<String> behaviourTags = Collections.emptyList();
   @Nullable private Long startTime;
   @Nullable private Long endTime;
   private long createdAt;
